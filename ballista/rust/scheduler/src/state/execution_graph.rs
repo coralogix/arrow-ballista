3a6bc441ac4c97c2e9e6363d880225edee8cf674
Faiaz Sanaulla <faiaz.sanaulla@coralogix.com>
2022-10-24 15:04:47 +0300

    type

--- ballista/rust/scheduler/src/state/execution_graph.rs
+++ ballista/rust/scheduler/src/state/execution_graph.rs
@@ -169,7 +169,7 @@ impl ExecutionGraph {
     }
 
     /// Revive the execution graph by converting the resolved stages to running stages
-    /// If any stages are converted, return amount of newly converted states.
+    /// If any stages are converted, return amount of newly converted stages.
     pub fn revive(&mut self) -> usize {
         let running_stages = self
             .stages
