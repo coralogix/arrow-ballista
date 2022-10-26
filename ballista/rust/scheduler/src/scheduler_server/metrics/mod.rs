f6fcb2dffd031f396bd5c24ae5b6accc13a5a295
Faiaz Sanaulla <faiaz.sanaulla@coralogix.com>
2022-10-26 16:17:14 +0300

    sc-5792: update method signature

--- ballista/rust/scheduler/src/scheduler_server/metrics/mod.rs
+++ ballista/rust/scheduler/src/scheduler_server/metrics/mod.rs
@@ -2,7 +2,7 @@ pub trait SchedulerMetricsCollector: Send + Sync {
     fn record_submitted(&self, job_id: &str, queued_at: u64, submitted_at: u64);
     fn record_completed(&self, job_id: &str, queued_at: u64, completed_at: u64);
     fn record_failed(&self, job_id: &str, queued_at: u64, failed_at: u64);
-    fn set_pending_tasks_queue(&self, sz: usize);
+    fn set_pending_tasks_queue(&self, value: f64);
 }
 
 #[derive(Default)]
@@ -12,5 +12,5 @@ impl SchedulerMetricsCollector for NoopMetricsCollector {
     fn record_submitted(&self, _job_id: &str, _queued_at: u64, _submitted_at: u64) {}
     fn record_completed(&self, _job_id: &str, _queued_at: u64, _completed_att: u64) {}
     fn record_failed(&self, _job_id: &str, _queued_at: u64, _failed_at: u64) {}
-    fn set_pending_tasks_queue(&self, _sz: usize) {}
+    fn set_pending_tasks_queue(&self, _value: f64) {}
 }
