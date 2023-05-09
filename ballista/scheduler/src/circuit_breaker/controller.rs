0b49d034d30314ca3ebc78db7bd50511e49b4b02
Markus Appel <markusappel@hotmail.de>
2023-05-09 12:51:46 +0200

    Improve code

--- ballista/scheduler/src/circuit_breaker/controller.rs
+++ ballista/scheduler/src/circuit_breaker/controller.rs
@@ -1,8 +1,9 @@
 use std::collections::HashMap;
 
 use ballista_core::serde::protobuf::CircuitBreakerKey;
+use log::warn;
 use parking_lot::RwLock;
-use tracing::{info, warn};
+use tracing::info;
 
 pub struct CircuitBreakerController {
     job_states: RwLock<HashMap<String, JobState>>,
@@ -83,72 +84,47 @@ impl CircuitBreakerController {
 
     pub fn update(&self, key: CircuitBreakerKey, percent: f64) -> Result<bool, String> {
         let mut job_states = self.job_states.write();
+
         let stage_states = match job_states.get_mut(&key.job_id) {
             Some(state) => &mut state.stage_states,
-            // If the registration hasn't happened yet
             None => {
                 warn!(
-                    "Circuit breaker update received for unregistered job {}",
+                    "Received circuit breaker update for unregisterd job {}",
                     key.job_id
                 );
                 return Ok(false);
             }
         };
 
-        let attempt_states = match stage_states.get_mut(&key.stage_id) {
-            Some(state) => &mut state.attempt_states,
-            None => {
-                let attempt_states = HashMap::new();
-                &mut stage_states
-                    .entry(key.stage_id)
-                    .or_insert_with(|| StageState { attempt_states })
-                    .attempt_states
-            }
-        };
+        let partition_states = &mut stage_states
+            .entry(key.stage_id)
+            .or_insert_with(|| StageState {
+                attempt_states: HashMap::new(),
+            })
+            .attempt_states
+            .entry(key.attempt_num)
+            .or_insert_with(|| AttemptState {
+                node_states: HashMap::new(),
+            })
+            .node_states
+            .entry(key.node_id.to_owned())
+            .or_insert_with(|| NodeState {
+                partition_states: HashMap::new(),
+            })
+            .partition_states;
 
-        let node_states = match attempt_states.get_mut(&key.attempt_num) {
-            Some(state) => &mut state.node_states,
-            None => {
-                let node_states = HashMap::new();
-                &mut attempt_states
-                    .entry(key.attempt_num)
-                    .or_insert_with(|| AttemptState { node_states })
-                    .node_states
-            }
-        };
+        partition_states
+            .entry(key.partition)
+            .or_insert_with(|| PartitionState { percent })
+            .percent = percent;
 
-        let partition_states = match node_states.get_mut(&key.node_id) {
-            Some(state) => &mut state.partition_states,
-            None => {
-                let partition_states = HashMap::new();
-                &mut node_states.entry(key.node_id)
-                    .or_insert_with(|| NodeState { partition_states })
-                    .partition_states
-            }
-        };
+        let should_trip =
+            partition_states.values().map(|s| s.percent).sum::<f64>() >= 1.0;
 
-        match partition_states.get_mut(&key.partition) {
-            Some(mut state) => {
-                state.percent = percent;
-            }
-            None => {
-                partition_states.insert(key.partition, PartitionState { percent });
-            }
-        };
-
-        let should_circuit_breaker = partition_states
-            .values()
-            .map(|s| s.percent)
-            .fold(0.0, |a, b| a + b)
-            >= 1.0;
-
-        if should_circuit_breaker {
-            info!(
-                "Sending circuit breaker signal to task {}, global limit reached",
-                key.task_id
-            );
+        if should_trip {
+            info!("Sending circuit breaker signal to task {}", key.task_id);
         }
 
-        Ok(should_circuit_breaker)
+        Ok(should_trip)
     }
 }
