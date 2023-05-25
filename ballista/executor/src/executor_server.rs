d9ac16a7ca4604799538429403818c8d274782e0
Dan Harris <dan@coralogix.com>
2023-05-25 08:50:29 -0400

    fix comment

--- ballista/executor/src/executor_server.rs
+++ ballista/executor/src/executor_server.rs
@@ -1061,7 +1061,7 @@ impl<T: 'static + AsLogicalPlan, U: 'static + AsExecutionPlan> ExecutorGrpc
         &self,
         request: Request<LaunchTaskParams>,
     ) -> Result<Response<LaunchTaskResult>, Status> {
-        // Reject new tasks is already in TERMINATING state
+        // Reject new tasks if in TERMINATING state
         if TERMINATING.load(Ordering::Acquire) {
             return Err(Status::unavailable("executor is terminating"));
         }
