5185a13bdce6b26cb6bc443bae6389eea8383fdb
Faiaz Sanaulla <105630300+fsdvh@users.noreply.github.com>
2023-11-21 16:16:13 +0100

    Update ballista/core/src/replicator/mod.rs
    Co-authored-by: Dan Harris <1327726+thinkharderdev@users.noreply.github.com>

--- ballista/core/src/replicator/mod.rs
+++ ballista/core/src/replicator/mod.rs
@@ -52,8 +52,8 @@ pub async fn start_replication(
                     warn!("Failed to open file for replication: {}", path);
                 }
             },
-            Err(_) => {
-                warn!("Failed to parse replication path: {}", path);
+            Err(error) => {
+                warn!(?error, ?path, "Failed to parse replication path");
             }
         }
     }
