2cc8c9b0e559b24cd2df523b645ccdff6449f165
Faiaz Sanaulla <faiaz.sanaulla@coralogix.com>
2023-11-28 21:35:56 +0000

    VTX-3411: do async write to object store

--- ballista/core/src/replicator/mod.rs
+++ ballista/core/src/replicator/mod.rs
@@ -1,12 +1,16 @@
 use std::sync::Arc;
 
 use bytes::Bytes;
+use datafusion::arrow::ipc::writer::{IpcWriteOptions, StreamWriter};
+use datafusion::arrow::ipc::CompressionType;
 use object_store::{path::Path, ObjectStore};
-use tokio::io::AsyncReadExt;
+use tokio::io::AsyncWriteExt;
 use tokio::{fs::File, sync::mpsc};
 use tracing::warn;
 
+use crate::client::AsyncStreamReader;
 use crate::error::BallistaError;
+use tokio_util::compat::TokioAsyncReadCompatExt;
 
 pub enum Command {
     Replicate { path: String },
@@ -20,41 +24,105 @@ pub async fn start_replication(
     while let Some(Command::Replicate { path }) = receiver.recv().await {
         match Path::parse(base_path.as_str()) {
             Ok(location) => match File::open(path.as_str()).await {
-                Ok(mut file) => {
-                    let size = file.metadata().await.map_or(1024, |m| m.len());
-                    let mut content = Vec::with_capacity(size as usize);
+                Ok(file) => match AsyncStreamReader::try_new(file.compat(), None).await {
+                    Ok(mut reader) => match object_store.put_multipart(&location).await {
+                        Ok((_, mut multipart_writer)) => {
+                            while let Some(batch) = reader.maybe_next().await.transpose()
+                            {
+                                match batch {
+                                    Ok(batch) => {
+                                        let mut serialized_data = Vec::with_capacity(
+                                            batch.get_array_memory_size(),
+                                        );
+                                        let options = match IpcWriteOptions::default()
+                                            .try_with_compression(Some(
+                                                CompressionType::LZ4_FRAME,
+                                            )) {
+                                            Err(error) => {
+                                                warn!(
+                                                    ?error,
+                                                    "Failed to enable compression"
+                                                );
+                                                IpcWriteOptions::default()
+                                            }
+                                            Ok(opt) => opt,
+                                        };
 
-                    match file.read_to_end(&mut content).await {
+                                        {
+                                            match StreamWriter::try_new_with_options(
+                                                &mut serialized_data,
+                                                batch.schema().as_ref(),
+                                                options,
+                                            ) {
+                                                Ok(mut writer) => {
+                                                    if let Err(error) =
+                                                        writer.write(&batch)
+                                                    {
+                                                        warn!(?error, ?path, "Failed to write batch to a stream writer");
+                                                        break;
+                                                    }
+
+                                                    if let Err(error) = writer.finish() {
+                                                        warn!(
+                                                            ?error,
+                                                            ?path,
+                                                            "Failed to finish writing to a stream writer");
+                                                        break;
+                                                    }
+                                                }
+                                                Err(error) => {
+                                                    warn!(
+                                                        ?error,
+                                                        ?path,
+                                                        "Failed to build a stream writer"
+                                                    );
+                                                    break;
+                                                }
+                                            }
+                                        }
+
+                                        let bytes = Bytes::from(serialized_data);
+                                        if let Err(error) =
+                                            multipart_writer.write_all(&bytes).await
+                                        {
+                                            warn!(
+                                                ?error,
+                                                ?path,
+                                                "Failed to write batch to object store"
+                                            );
+                                            break;
+                                        }
+                                    }
+                                    Err(error) => {
+                                        warn!(
+                                            ?error,
+                                            ?path,
+                                            "Failed to write retrieve batch from stream"
+                                        );
+                                        break;
+                                    }
+                                }
+                            }
+
+                            if let Err(error) = multipart_writer.flush().await {
+                                warn!(?error, ?path, "Failed to flush writer")
+                            }
+                            if let Err(error) = multipart_writer.shutdown().await {
+                                warn!(?error, ?path, "Failed to shutdown writer")
+                            }
+                        }
                         Err(error) => {
                             warn!(
                                 ?error,
                                 ?path,
-                                "Failed to read content of the file for replication"
+                                "Failed to create obejct store multipart writer"
                             );
                         }
-                        _ => match Path::parse(format!("{}/{}", base_path, location)) {
-                            Ok(final_path) => {
-                                if let Err(error) = object_store
-                                    .put(&final_path, Bytes::from(content))
-                                    .await
-                                {
-                                    warn!(
-                                        ?error,
-                                        ?final_path,
-                                        "Failed to replicate file to object store"
-                                    );
-                                }
-                            }
-                            Err(error) => {
-                                warn!(
-                                    ?error,
-                                    ?path,
-                                    "Failed to parse final replication path"
-                                );
-                            }
-                        },
+                    },
+                    Err(error) => {
+                        warn!(?error, ?path, "Failed to create async reader");
                     }
-                }
+                },
                 Err(error) => {
                     warn!(?error, ?path, "Failed to open file for replication");
                 }
