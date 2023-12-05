1f148dd37c539e3fa16ee10a247171af9d8dabe3
Faiaz Sanaulla <faiaz.sanaulla@coralogix.com>
2023-12-05 11:31:17 +0100

    VTX-3411: fmt

--- ballista/core/src/execution_plans/mod.rs
+++ ballista/core/src/execution_plans/mod.rs
@@ -26,8 +26,8 @@ mod unresolved_shuffle;
 
 pub use coalesce_tasks::CoalesceTasksExec;
 pub use distributed_query::DistributedQueryExec;
-pub use shuffle_reader::fetch_partition_object_store;
 pub use shuffle_reader::batch_stream_from_object_store;
+pub use shuffle_reader::fetch_partition_object_store;
 pub use shuffle_reader::ShuffleReaderExec;
 pub use shuffle_writer::ShuffleWriterExec;
 pub use unresolved_shuffle::UnresolvedShuffleExec;
