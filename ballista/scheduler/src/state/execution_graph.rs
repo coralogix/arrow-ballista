b0b836d393ba9c19420c44730c640428166ff56c
Christoph Schulze <ch-sc@users.noreply.github.com>
2024-02-16 17:12:41 +0100

    VTX-3877 Priority-based Scheduling (#186)
    * VTX-3877 priority based scheduling

--- ballista/scheduler/src/state/execution_graph.rs
+++ ballista/scheduler/src/state/execution_graph.rs
@@ -374,9 +374,9 @@ impl ExecutionGraph {
         )
     }
 
-    // If executor is None,
-    // we do not collect output partitions.
-    // E.g. in case of circuit breaker.
+    /// If executor is None,
+    /// we do not collect output partitions.
+    /// E.g. in case of circuit breaker.
     fn update_task_status_internal(
         &mut self,
         executor: Option<&ExecutorMetadata>,
@@ -980,7 +980,7 @@ impl ExecutionGraph {
             .collect::<Vec<_>>()
     }
 
-    // Return all currently running tasks along with the executor ID on which they are assigned
+    /// Return all currently running tasks along with the executor ID on which they are assigned
     pub fn running_tasks(&self) -> Vec<RunningTaskInfo> {
         let mut tasks = Vec::default();
 
@@ -1002,7 +1002,7 @@ impl ExecutionGraph {
         tasks
     }
 
-    // Total number of tasks in this plan that are ready for scheduling
+    /// Total number of tasks in this plan that are ready for scheduling
     pub fn available_tasks(&self) -> usize {
         self.stages
             .values()
@@ -1030,7 +1030,7 @@ impl ExecutionGraph {
         if matches!(
             self.status,
             JobStatus {
-                status: Some(job_status::Status::Failed(_)),
+                status: Some(Status::Failed(_)),
                 ..
             }
         ) {
